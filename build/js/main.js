c732835ee9414ba91a1337e9405ba55d0d8d993a
sanyokstd <59456430+sanyokstd@users.noreply.github.com>
2021-03-01 14:41:18 +0200

    1
    1

--- build/js/main.js
+++ build/js/main.js
@@ -1,4 +1,5 @@
 $(function() {
+
     $('.avatar .avatar__img').on('click', function() {
         $('.menu').fadeToggle(200)
     })
@@ -37,10 +38,41 @@ $(function() {
 
     $('.nise-select').niceSelect()
 
-
+    // маркет
     $('.market__right .btn-api').on('click', function() {
         $('.market__item').removeClass('active')
         $(this).closest('.market__item').addClass('active')
     })
 
+    $('.market__right .btn-save').on('click', function() {
+        $('.market__item').removeClass('active')
+    })
+
+    $('.market__item .checkbox-inline input').on('change', function() {
+        if (!$(this).is(":checked")) {
+            $(this).closest('.market__item').addClass('not-active')
+        } else {
+            $(this).closest('.market__item').removeClass('not-active')
+        }
+    })
+
+    // каталог
+    $('.action__wrap').on('click', function() {
+        $('.action__toggle').fadeToggle(200)
+    })
+
+    $('.action__toggle__item').on('click', function() {
+
+        $('.action__item').removeClass('active')
+        $('.action__item').eq($(this).index()).addClass('active')
+    })
+
+    $(document).mouseup(function(e) { // событие клика по веб-документу
+        var div = $('.action__wrap'); // тут указываем ID элемента
+        if (!div.is(e.target) // если клик был не по нашему блоку
+            &&
+            div.has(e.target).length === 0) { // и не по его дочерним элементам
+            $('.action__toggle').fadeOut(200)
+        }
+    });
 })
