00fc82f04f0943d3b98c9f67700b3a6f15918ef6
sanyokstd <59456430+sanyokstd@users.noreply.github.com>
2021-03-02 13:47:21 +0200

    1
    1

--- build/js/main.js
+++ build/js/main.js
@@ -75,4 +75,107 @@ $(function() {
             $('.action__toggle').fadeOut(200)
         }
     });
+
+
+
+    $('.catalog__setting__btn').on('click', function() {
+        $(this).closest('.catalog__settign').find('.catalog__setting__wrap').fadeToggle(200)
+    })
+    $(document).mouseup(function(e) { // событие клика по веб-документу
+        var div = $('.catalog__settign'); // тут указываем ID элемента
+        if (!div.is(e.target) // если клик был не по нашему блоку
+            &&
+            div.has(e.target).length === 0) { // и не по его дочерним элементам
+            $('.catalog__setting__wrap').fadeOut(200)
+        }
+    });
+    console.log($('.catalog__head .action input[type="checkbox"]'))
+    $('.catalog__head .action input[type="checkbox"]').on('change', function() {
+
+        if ($(this).is(":checked")) {
+            $('.catalog__item input[type="checkbox"]').prop('checked', true);
+        } else {
+            $('.catalog__item input[type="checkbox"]').prop('checked', false);
+        }
+    })
+
+    // логика фильтра каталога
+    let priceItem = $('.catalog__price .b-item')
+
+    let bodyItem = $('.catalog__body__row1 .b-item')
+
+    let headItem = $('.catalog__head .b-item')
+
+    let priceRow = $('.catalog__price')
+
+    let settingCheck = $('.catalog__settign ul input[type="checkbox"]')
+
+    let priceCheck = $('.catalog__settign .catalog__setting__price input[type="checkbox"]')
+
+    let arrSetting = [];
+
+    function settingReed() {
+        let count = 0;
+        $.each(settingCheck, function(i, val) {
+            if ($(val).is(":checked")) {
+                arrSetting[i] = 1;
+                count++
+            } else {
+                arrSetting[i] = 0;
+            }
+        })
+        if (count > 2) {
+            $('.catalog__table').addClass('big')
+        } else {
+            $('.catalog__table').removeClass('big')
+        }
+    }
+
+    function settingApruv() {
+        let j = 0;
+        $.each(priceItem, function(i, val) {
+            if (j > 3) j = 0
+            if (arrSetting[j]) {
+                $(val).show()
+            } else {
+                $(val).hide()
+            }
+            j++
+        })
+
+        $.each(bodyItem, function(i, val) {
+            if (j > 3) j = 0
+            if (arrSetting[j]) {
+                $(val).show()
+            } else {
+                $(val).hide()
+            }
+            j++
+        })
+        $.each(headItem, function(i, val) {
+            if (arrSetting[i]) {
+                $(val).show()
+            } else {
+                $(val).hide()
+            }
+        })
+
+        if (priceCheck.is(":checked")) {
+            priceRow.show()
+        } else {
+            priceRow.hide()
+        }
+    }
+
+    settingCheck.on('change', () => {
+        settingReed()
+        settingApruv()
+    })
+    priceCheck.on('change', () => {
+        settingReed()
+        settingApruv()
+    })
+
+    settingReed()
+    settingApruv()
 })
